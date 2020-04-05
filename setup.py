44499872178ce3f3b4ba8aa7568c901136543db8
Peter Grayson <pete@jpgrayson.net>
2020-04-05 15:56:31 -0400

    Build system updates for PEP-517/518
    The distribution metadata is migrated from setup.py to setup.cfg. setup.py
    is now just a stub. Note that `python setup.py ...` commands still work.
    
    A pyproject.toml file is added. It contains the PEP-517/518 specified
    build-system section which describes SimPy's build-time dependencies along
    with which build backend SimPy uses (which is still setuptools).
    
    And tox is now configured with isolated_build = True, which causes tox to
    make the source dist in a sandbox instead of relying on the host
    environment. This helps ensure that the distribution build will work with
    just the tooling specified in pyproject.toml.

--- setup.py
+++ setup.py
@@ -1,42 +1,4 @@
 # encoding: utf-8
-from setuptools import setup, find_packages
+from setuptools import setup
 
-
-setup(
-    name='simpy',
-    author='Ontje Lünsdorf, Stefan Scherfke',
-    author_email='the_com@gmx.de, stefan@sofa-rockers.org',
-    description='Event discrete, process based simulation for Python.',
-    long_description='\n\n'.join(
-        open(f, 'rb').read().decode('utf-8')
-        for f in ['README.rst', 'CHANGES.rst', 'AUTHORS.rst']),
-    long_description_content_type='text/x-rst',
-    url='https://simpy.readthedocs.io',
-    license='MIT License',
-    use_scm_version=True,
-    setup_requires=['setuptools_scm'],
-    install_requires=[],
-    python_requires='>= 3.6',
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
-    include_package_data=True,
-    zip_safe=False,
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Education',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: Scientific/Engineering',
-    ],
-)
+setup(use_scm_version=True)
